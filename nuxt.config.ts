80e945a9434812d06abc6c92dd8c95092d96a3d6
Sefa Bulak <zckhtln@icloud.com>
2024-08-21 10:43:31 +0300

    added plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ['nuxt-graphql-request'],
+  graphql: {
+    clients: {
+      default: {
+        endpoint: 'https://nuxtcommerce.zackha.com/px1dev66mvodt7j',
+      },
+    },
+  },
+});
